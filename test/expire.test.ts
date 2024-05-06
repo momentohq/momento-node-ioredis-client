6eaabd543b6f017fa02da053f3d452475532a877
eaddingtonwhite <5491827+ellery44@users.noreply.github.com>
2024-05-06 13:41:13 -0700

    feat: adds support for expire command

--- test/expire.test.ts
+++ test/expire.test.ts
@@ -188,3 +188,189 @@ describe('pexpire', () => {
     expect(pexpireRsp).toBe(0);
   });
 });
+
+describe('expire', () => {
+  it('should update ttl using expire when no flags are specified', async () => {
+    const key = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value);
+
+    // Set ttl of key
+    const expireRsp = await client.expire(key, 5);
+    expect(expireRsp).toBe(1);
+
+    // Get ttl of key
+    const ttlResp = await client.ttl(key);
+    expect(ttlResp).toBeGreaterThan(2);
+  });
+
+  it('should overwrite ttl using expire when no flags are specified', async () => {
+    const key = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 5);
+
+    // Set ttl of key
+    const expireRsp = await client.expire(key, 1);
+    expect(expireRsp).toBe(1);
+
+    // Get ttl of key
+    const ttlResp = await client.ttl(key);
+    expect(ttlResp).toBeGreaterThan(5);
+  });
+
+  it('should not update ttl using expire with nx flag when key exists when momento enabled', async () => {
+    if (process.env.MOMENTO_ENABLED === 'true') {
+      const key = v4();
+      const value = v4();
+
+      // Set initial key value
+      await client.set(key, value);
+
+      // Set ttl of key using nx flag
+      const expireRsp = await client.expire(key, 5, 'NX');
+      expect(expireRsp).toBe(0);
+    }
+  });
+
+  it('should update ttl using expire with nx flag when key exists with no ttl when redis enabled', async () => {
+    if (process.env.REDIS_ENABLED === 'true') {
+      const key = v4();
+      const value = v4();
+
+      // Set initial key value
+      await client.set(key, value);
+
+      // Set ttl of key using nx flag
+      const expireRsp = await client.expire(key, 5, 'NX');
+      expect(expireRsp).toBe(1);
+    }
+  });
+
+  it('should not update ttl using expire with nx flag when key exists and ttl on key exists', async () => {
+    const key = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 5);
+
+    // Set ttl of key using nx flag
+    const expireRsp = await client.expire(key, 10, 'NX');
+    expect(expireRsp).toBe(0);
+  });
+
+  it('should not update ttl when expire with nx flag when key does not exist', async () => {
+    const key = v4();
+    const key1 = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 5);
+
+    // Set ttl of key using nx flag
+    const expireRsp = await client.expire(key1, 10, 'NX');
+    expect(expireRsp).toBe(0);
+  });
+
+  it('should update ttl using expire with xx flag is specified when key exists and ttl on key exits', async () => {
+    const key = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 5);
+
+    // Set ttl of key using xx flag
+    const expireRsp = await client.expire(key, 10, 'XX');
+    expect(expireRsp).toBe(1);
+  });
+
+  it('should not update ttl using expire with xx flag when key does not exist', async () => {
+    const key = v4();
+    const key1 = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 5);
+
+    // Set ttl of key using xx flag
+    const expireRsp = await client.expire(key1, 10, 'XX');
+    expect(expireRsp).toBe(0);
+  });
+
+  it('should update ttl using expire with gt flag when key exists and new expiry is greater than current one', async () => {
+    const key = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 5);
+
+    // Set ttl of key using gt flag
+    const expireRsp = await client.expire(key, 10, 'GT');
+    expect(expireRsp).toBe(1);
+  });
+
+  it('should not update ttl using expire with gt flag when key exists and new expiry is less than the current one', async () => {
+    const key = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 10);
+
+    // Set ttl of key using gt flag
+    const expireRsp = await client.expire(key, 5, 'GT');
+    expect(expireRsp).toBe(0);
+  });
+
+  it('should not update ttl using expire with gt flag when key does not exist', async () => {
+    const key = v4();
+    const key1 = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 10);
+
+    // Set ttl of key using gt flag
+    const expireRsp = await client.expire(key1, 5, 'GT');
+    expect(expireRsp).toBe(0);
+  });
+
+  it('should update ttl using expire with lt flag when key exists and new expiry is less than current one', async () => {
+    const key = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 10);
+
+    // Set ttl of key using lt flag
+    const expireRsp = await client.expire(key, 5, 'LT');
+    expect(expireRsp).toBe(1);
+  });
+
+  it('should not update ttl using expire with lt flag when key exists and new expiry is greater than current one', async () => {
+    const key = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 5);
+
+    // Set ttl of key using lt flag
+    const expireRsp = await client.expire(key, 10, 'LT');
+    expect(expireRsp).toBe(0);
+  });
+
+  it('should not update ttl using expire with lt flag when key does not exist', async () => {
+    const key = v4();
+    const key1 = v4();
+    const value = v4();
+
+    // Set initial key value
+    await client.set(key, value, 'EX', 5);
+
+    // Set ttl of key using lt flag
+    const expireRsp = await client.expire(key1, 10, 'LT');
+    expect(expireRsp).toBe(0);
+  });
+});
